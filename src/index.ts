18d8c23e8f632d5ed0c17c5e6506ea361367950b
voruti <45130096+voruti@users.noreply.github.com>
2021-11-29 21:04:39 +0100

    Log individual service status

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { Config } from "./config/config.js";
 
 console.log("Checking every service...");
-Promise.all(Config.services.map((service) => service.check())).then((values) =>
-  console.log("All services up:", values.every(Boolean))
+Promise.all(
+  Config.services.map((service) => {
+    const promise = service.check();
+    promise.then((result) => console.log(service.getName(), "up:", result));
+    return promise;
+  })
+).then((values) =>
+  console.log("===========", "\nAll services up:", values.every(Boolean))
 );
